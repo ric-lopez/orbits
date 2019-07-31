1fc7fdade6ba920b31c2c7261de6f2c7fafaeb82
ric-lopez <riclopez@ciencias.unam.mx>
2019-07-31 16:19:29 -0500

    calcula orbitas sobre un convexo. Error en la funcion calc_tangente

--- orbitas.py
+++ orbitas.py
@@ -7,6 +7,7 @@ from matplotlib import collections as mc
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 import random
 import math
+import sys
 
 class point:
     
@@ -111,7 +112,6 @@ def cierre_convexo(puntos):
     ch = ConvexHull(puntos_np)
     v_lista = []
     for v in ch.vertices:
-        print(ch.points[v])
         v_lista.append(point(ch.points[v][0],ch.points[v][1]))
     return v_lista
 
@@ -139,12 +139,27 @@ def orbita_convexo(polygon, p, a, direccion):
         print(t_der)
         print(vuelta2)
         print(t_izq)
-    #vuelta2 = lado_p_a_segmento(tangentes[1], p, tangentes[0])
-    return
+    
+    if(direccion == "derecha"):
+        orbit = []
+        pi = p
+        qi = t_der
+        for i in range(5):
+            pj = antipodal(pi, qi, a)
+            print("p: "+str(pi)+" q: "+str(qi)+" antipodal: "+str(pj))
+            orbit.append((pi, qi, pj))
+            tangentes = calc_tangente(polygon, pj)
+            pi = pj
+            if(qi == tangentes[0]):
+                qi = tangentes[1]
+            else:
+                qi = tangentes[0]
+
+    return orbit
 
 
 
-def dibuja(puntos):
+def dibuja(orbita):
     colors = (0,0,0)
     area = np.pi*3
     # Plot
@@ -153,29 +168,19 @@ def dibuja(puntos):
     lineas = []
     x1 = []
     y1 = []
-    ch = []
-    for p in puntos:
+    #ch = []
+    for p in orbita:
         x1.append(p[1].x)
         x.append(p[2].x)
         y1.append(p[1].y)
         y.append(p[2].y)
         lineas.append([(p[0].x, p[0].y), (p[2].x, p[2].y)])
-        ch.append([p[1].x, p[1].y])
+        #ch.append([p[1].x, p[1].y])
 
     lc = mc.LineCollection(lineas, linewidths=1)
     fig, ax = plt.subplots()
     ax.add_collection(lc)
-    #ax.autoscale()
     ax.margins(0,1)
-    #print(x)
-    #print(y)
-
-    puntos_np = np.array(ch)
-    #puntos_np = np.random.rand(5,2)
-    convexo = ConvexHull(puntos_np)
-    for x in convexo:
-        plt.plot(puntos_np[x, 0], puntos_np[x, 1], 'k-')
-
 
     plt.scatter(x, y, s=area, c=colors, alpha=0.5)
     plt.scatter(x1, y1, s=area, c='red', alpha=0.5)
@@ -185,88 +190,158 @@ def dibuja(puntos):
     plt.show()
 
 
-# Create data
-ps = []
-ps_lista = []
-for x in range(5):
-    p_new = point(random.randrange(20), random.randrange(20))
-    ps.append(p_new)
-    ps_lista.append([p_new.x, p_new.y])
+def dibuja_convexo(polygon, orbita):
 
-#mi_orbita = orbita(ps, point(random.randrange(20), random.randrange(20)), 1)
-#dibuja(mi_orbita)
+    # plot
+    colors = (0,0,0)
+    area = np.pi*3
+    x = []
+    y = []
+    ch = []
+    # polygon
+    for p in polygon:
+        ch.append([p.x, p.y])
+        x.append(p.x)
+        y.append(p.y)
+        #lineas.append([(p.x, p.y), (p[2].x, p[2].y)])
 
+    points = np.array(ch)
+    lineas =[]
+    for i in range(len(points)-1):
+        lineas.append([(points[i][0],points[i][1]), (points[i+1][0], points[i+1][1])])
+    lineas.append([(points[-1][0],points[-1][1]), (points[0][0], points[0][1])])
 
-print("tangentes")
-print("-----------------------------------------------------------")
-#print(type(cierre_convexo(ps_lista)))
-#print(cierre_convexo(ps_lista))
-mi_ch = cierre_convexo(ps_lista)    
-#p_inicio = point(random.randrange(20), random.randrange(20))
-p_inicio = point(25, 1)
-print(p_inicio)
-print("------------------------")
-for aux in mi_ch:
-    print(str(aux))
-print("------------------------")    
-angulos = calc_tangente(mi_ch, p_inicio)
-print(angulos[0])
-print(angulos[1])
-print("-------------------------")
-orbita_convexo(mi_ch, p_inicio, 1, "derecha")
+    #orbita
+    x1 = []
+    y1 = []
+    lineas_o = []
+    for p in orbita:
+        x1.append(p[0].x)
+        y1.append(p[0].y)
+        lineas_o.append([(p[0].x, p[0].y), (p[2].x, p[2].y)])
 
-# print("------------------------------------------------------")
-# print("sobre el ejex")
-# p1 = point(1,0)
-# p2 = point(3,0)
-# print("p1: "+str(p1))
-# print("p2: "+str(p2))
-# print(antipodal(p1,p2,1))
-# p1 = point(3,0)
-# p2 = point(1,0)
-# print("p1: "+str(p1))
-# print("p2: "+str(p2))
-# print(antipodal(p1,p2,1))
-# print("------------------------------------------------------")
-# print("cuadrante I")
-# p1 = point(3,5)
-# p2 = point(1,0)
-# print("p1: "+str(p1))
-# print("p2: "+str(p2))
-# print(antipodal(p1,p2,1))
-# print("------------------------------------------------------")
-# print("cuadrante II")
-# p1 = point(-3,2)
-# p2 = point(1,0)
-# print("p1: "+str(p1))
-# print("p2: "+str(p2))
-# print(antipodal(p1,p2,1))
-# print("------------------------------------------------------")
-# print("cuadrante III")
-# p1 = point(-3,-3)
-# p2 = point(1,0)
-# print("p1: "+str(p1))
-# print("p2: "+str(p2))
-# print(antipodal(p1,p2,1))
-# print("------------------------------------------------------")
-# print("cuadrante IV")
-# p1 = point(3,-2)
-# p2 = point(1,0)
-# print("p1: "+str(p1))
-# print("p2: "+str(p2))
-# print(antipodal(p1,p2,1))
-# print("------------------------------------------------------")
-# print("puntos iguales")
-# p1 = point(1,0)
-# p2 = point(1,0)
-# print("p1: "+str(p1))
-# print("p2: "+str(p2))
-# print(antipodal(p1,p2,1))
+    lc_o = mc.LineCollection(lineas_o, linewidths=1)
+    lc = mc.LineCollection(lineas, colors="red", linewidths=1)
+    fig, ax = plt.subplots()
+    
+    ax.add_collection(lc_o)
+    ax.add_collection(lc)
+    ax.margins(0,1)
+
+    # puntos
+    plt.scatter(x, y, s=area, c="red", alpha=0.5)
+    plt.scatter(x1, y1, s=area, c=colors, alpha=0.5)
+    plt.title('Orbit')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
 
 
 
+def main(n, r, tipo):
+    """
+    n -> cantidad de puntos
+    r -> rango de valores de los puntos
+    tipo -> tipo de orbita a calcular
+    """
+    if(tipo == "puntos"):
+        # Create data
+        ps = []
+        ps_lista = []
+        for x in range(n):
+            p_new = point(random.randrange(r), random.randrange(r))
+            ps.append(p_new)
+            ps_lista.append([p_new.x, p_new.y])
+        # plot orbita
+        mi_orbita = orbita(ps, point(random.randrange(r), random.randrange(r)), 1)
+        dibuja(mi_orbita)
+    elif(tipo == "convexo"):
+        # Create data
+        ps = []
+        ps_lista = []
+        for x in range(n):
+            p_new = point(random.randrange(r), random.randrange(r))
+            ps.append(p_new)
+            ps_lista.append([p_new.x, p_new.y])
+
+        mi_ch = cierre_convexo(ps_lista)    
+        p_inicio = point(random.randrange(20), random.randrange(20))
+        #p_inicio = point(25, 1)
+        
+        print("------------------------")
+        print(p_inicio)
+        print("------------------------")
+        for aux in mi_ch:
+            print(str(aux))
+        print("------------------------")    
+        angulos = calc_tangente(mi_ch, p_inicio)
+        print(angulos[0])
+        print(angulos[1])
+        print("-------------------------")
 
 
+        mi_orbita = orbita_convexo(mi_ch, p_inicio, 1, "derecha")
+        dibuja_convexo(mi_ch, mi_orbita)
+
+    else:
+        print("tipo de orbita no valida")
+
+
+
+
+    
+
+
+if __name__ == '__main__':
+    main(int(sys.argv[1]), int(sys.argv[2]), str(sys.argv[3]))
+
+    # print("------------------------------------------------------")
+    # print("sobre el ejex")
+    # p1 = point(1,0)
+    # p2 = point(3,0)
+    # print("p1: "+str(p1))
+    # print("p2: "+str(p2))
+    # print(antipodal(p1,p2,1))
+    # p1 = point(3,0)
+    # p2 = point(1,0)
+    # print("p1: "+str(p1))
+    # print("p2: "+str(p2))
+    # print(antipodal(p1,p2,1))
+    # print("------------------------------------------------------")
+    # print("cuadrante I")
+    # p1 = point(3,5)
+    # p2 = point(1,0)
+    # print("p1: "+str(p1))
+    # print("p2: "+str(p2))
+    # print(antipodal(p1,p2,1))
+    # print("------------------------------------------------------")
+    # print("cuadrante II")
+    # p1 = point(-3,2)
+    # p2 = point(1,0)
+    # print("p1: "+str(p1))
+    # print("p2: "+str(p2))
+    # print(antipodal(p1,p2,1))
+    # print("------------------------------------------------------")
+    # print("cuadrante III")
+    # p1 = point(-3,-3)
+    # p2 = point(1,0)
+    # print("p1: "+str(p1))
+    # print("p2: "+str(p2))
+    # print(antipodal(p1,p2,1))
+    # print("------------------------------------------------------")
+    # print("cuadrante IV")
+    # p1 = point(3,-2)
+    # p2 = point(1,0)
+    # print("p1: "+str(p1))
+    # print("p2: "+str(p2))
+    # print(antipodal(p1,p2,1))
+    # print("------------------------------------------------------")
+    # print("puntos iguales")
+    # p1 = point(1,0)
+    # p2 = point(1,0)
+    # print("p1: "+str(p1))
+    # print("p2: "+str(p2))
+    # print(antipodal(p1,p2,1))
 
 
 
